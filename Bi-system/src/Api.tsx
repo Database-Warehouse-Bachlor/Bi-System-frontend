335b0fa98be09f061ba4ce34ffa9126c9e07e84c
PetterDesktop <pettersen_97@hotmail.com>
2021-03-08 12:51:42 +0100

    init Api calss

--- Bi-system/src/Api.tsx
+++ Bi-system/src/Api.tsx
@@ -1,29 +1,44 @@
-import React, { Component } from "react";
+import { useEffect, useState } from "react";
 
-export default class Api extends React.Component {
-    state = {
-        loading: true,
-        person: null,
-    };
-
-
- async componentDidMount() {
-    const url = "https://api.randomuser.me";
-    const response = await fetch(url)
-    const data = await response.json();
-    this.setState({person: data.results[0], loading: false})
-}
-    render() {
-        return (
-            <div>
-            {this.state.loading || !this.state.person ? (
-                <div>loading...</div>
-            ) : (
-                <div>
-                </div>
-            )}
-            </div>
-        );
+function MyComponent() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+      fetch("https://api.example.com/items")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
     }
-
-}
+  }
