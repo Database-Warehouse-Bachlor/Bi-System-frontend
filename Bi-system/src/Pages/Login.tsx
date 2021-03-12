2ccffff128bc1c44dce08d6203c7d69ec90454ea
Sverre Lofthus <sverrlof@stud.ntnu.no>
2021-03-12 10:53:32 +0100

    APi login call

--- Bi-system/src/Pages/Login.tsx
+++ Bi-system/src/Pages/Login.tsx
@@ -11,8 +11,9 @@ import {
 } from "@material-ui/core";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
-import { RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps, Router } from "react-router-dom";
 import { object, string } from "yup";
+
 
 
 const useStyles = makeStyles({
@@ -38,82 +39,104 @@ const useStyles = makeStyles({
 });
 
 
-interface Props extends RouteComponentProps {}
+interface Props extends RouteComponentProps { }
 
 export const Login: React.FC<Props> = ({ history }) => {
   const classes = useStyles();
   return (
-    <Grid container justify = "center" className={classes.loginCard}>
-    <Card className="LoginCard">
-      <CardHeader title="Login" className={classes.cardTitle} />
-      <CardContent>
-        <Formik
-          initialValues={{
-            Email: "",
-            APIKey: "",
-          }}
-          validationSchema={object({
-            Email: string().required().email(),
-            APIKey: string().required(),
-          })}
-          onSubmit={async (values) => {
-            /** A timer of 3 sec that disables the submit button - Somewhat prevents serverspam */
-            return new Promise<void>((res) => {
-              setTimeout(() => {
-                //API call & checks
-                console.log("my values", values);
-                history.push("/Dashboard");
+    <Grid container justify="center" className={classes.loginCard}>
+      <Card className="LoginCard">
+        <CardHeader title="Login" className={classes.cardTitle} />
+        <CardContent>
+          <Formik
+            initialValues={{
+              email: "",
+              pwd: "",
+            }}
+            validationSchema={object({
+              email: string().required().email(),
+              pwd: string().required(),
+            })}
+            
+            onSubmit={async (values) => {
+              /** A timer of 3 sec that disables the submit button - Somewhat prevents serverspam */
+                  //API call & checks
+                  fetch('/login', {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                    },
+                    body: new URLSearchParams({
+                      email: values.email,
+                      pwd: values.pwd
+                    })
+                  }).then(response => {
+                    console.log(response.text())
+                    return response;
+                  })
+                  .catch(error => console.log(error))
+                  console.log("my values", values);
+                }}
+          >
+            {({ values, errors, isSubmitting }) => (
+              <Form>
+                <div className={classes.loginForm}>
+                  <Field
+                    name="email"
+                    type="email"
+                    label="email"
+                    as={TextField}
+                  />
+                </div>
+                <ErrorMessage name="Email">
+                  {message => (
+                    <Typography color="error">{message}</Typography>
+                  )}
+                </ErrorMessage>
+                <div className={classes.loginForm}>
+                  <Field
+                    name="pwd"
+                    label="Password"
+                    as={TextField} />
+                  <ErrorMessage name="pwd">
+                    {message => (
+                      <Typography color="error">{message}</Typography>
+                    )}
+                  </ErrorMessage>
+                </div>
+                <Button
+                  className={classes.button}
+                  type="submit"
+                  variant="contained"
+                  disabled={isSubmitting}
+                  startIcon={
+                    isSubmitting ? <CircularProgress size="0.8rem" /> : undefined
+                  }
+                >
+                  {isSubmitting ? "Submitting" : "Login"}
+                </Button>
 
-                res();
-              }, 3000);
-            });
-          }}
-        >
-          {({ values, errors, isSubmitting }) => (
-            <Form>
-              <div className={classes.loginForm}>
-                <Field
-                  name="Email"
-                  type="Email"
-                  label="Email"
-                  as={TextField}
-                />
-              </div>
-              <ErrorMessage name ="Email">
-                {message => (
-                  <Typography color="error">{message}</Typography>
-                )}
-              </ErrorMessage>
-              <div className={classes.loginForm}>
-                <Field 
-                name="APIKey" 
-                label="API-Key" 
-                as={TextField} />
-                <ErrorMessage name ="APIKey">
-                {message => (
-                  <Typography color="error">{message}</Typography>
-                )}
-              </ErrorMessage>
-              </div>
-              <Button
-                className={classes.button}
-                type="submit"
-                variant="contained"
-                disabled={isSubmitting}
-                startIcon={
-                  isSubmitting ? <CircularProgress size="0.8rem" /> : undefined
-                }
-              >
-                {isSubmitting ? "Submitting" : "Login"}
-              </Button>
+              </Form>
+            )}
 
-            </Form>
-          )}
-          
-        </Formik>
-      </CardContent>
-    </Card>
+          </Formik>
+        </CardContent>
+      </Card>
     </Grid>
   );
 };
 export default Login;
+
+
+function setState(arg0: { email: any; pwd: any; }) {
+  throw new Error("Function not implemented.");
+}
+// function loginTest() {
+//   let headers = new Headers();
+
+//   headers.append('Access-Control-Allow-Origin', 'https://localhost:5001');
+//   headers.append('Access-Control-Allow-Credentials', 'true');
+
+//   headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   headers.append('Access-Control-Allow-Credentials', 'true');
+// }
