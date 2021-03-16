dcbf9abbff21ff41dd04fd6972c260b277e53ebb
PetterDesktop <pettersen_97@hotmail.com>
2021-03-16 12:10:24 +0100

    Added orgnr in user model

--- Bi-system/src/Models/User.tsx
+++ Bi-system/src/Models/User.tsx
@@ -1,11 +1,13 @@
 export class User {
 
     //Fields 
+    orgnr: string
     email: string
     password: string
     
     
-    constructor (email: string, password: string){
+    constructor (orgnr: string, email: string, password: string){
+       this.orgnr = orgnr
        this.email = email
        this.password = password
 
