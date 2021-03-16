dadc93774901e4c6f4482fb50f6f02201c624407
PetterDesktop <pettersen_97@hotmail.com>
2021-03-16 17:13:39 +0100

    Changed token in adduser

--- Bi-system/src/Services/AddUserService.tsx
+++ Bi-system/src/Services/AddUserService.tsx
@@ -10,7 +10,7 @@ function register(orgnr: string, email: string, pwd: string) {
   const requestOptions = {
     method: "POST",
     headers: new Headers({
-      Authorization: "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0MTIzNCIsImVtYWlsIjoicGV0dGVyQG1haWwuY29tIiwianRpIjoiNDc0NWRmN2YtZWVkYS00OTQzLThlMmQtNzZhNjZlNjgyYmM2IiwiZXhwIjoxNjE1OTEwMDgxLCJpc3MiOiJUZXN0LmNvbSIsImF1ZCI6IlRlc3QuY29tIn0.0q0nP7Exzv98wWU2oQNLRAWHWSrypJW6GCuvMPY3w30",
+      Authorization: "bearer " + AuthenticationService.getCurrentUser("currentUser"),
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     }),
     body: new URLSearchParams({ orgnr, email, pwd }),
