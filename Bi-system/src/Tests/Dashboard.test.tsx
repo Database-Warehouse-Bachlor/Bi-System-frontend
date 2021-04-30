219dc7cad5d4606534a9de1be8f6ded4ae672fd3
PetterDesktop <pettersen_97@hotmail.com>
2021-04-30 18:14:28 +0200

    added import

--- Bi-system/src/Tests/Dashboard.test.tsx
+++ Bi-system/src/Tests/Dashboard.test.tsx
@@ -1,5 +1,8 @@
 
-
+import React from 'react';
+import Dashboard from '../Pages/Dashboard';
+import { render, cleanup, waitFor, act, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 test("Renders Dashboard correctly", async () => {
   await act(async () => {
--- Bi-system/src/Tests/Login.test.tsx
+++ Bi-system/src/Tests/Login.test.tsx
@@ -1,3 +1,7 @@
+import  React  from  'react';
+import { act, cleanup, render } from  '@testing-library/react';
+import AuthenticationService from '../Services/AuthenticationService'
+import  Login from  '../Pages/Login';
 
 
 test('Renders Login correctly', async () => {
